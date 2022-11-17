3f7f4c1cefb26ed40d2507b70f4890d94f2dc20a
Michael Walsh <mpw9@bu.edu>
2022-11-17 17:43:01 -0500

    idk what I changed honestly

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
-const bknd = express();
+const { appendFile } = require("fs");
 
+const app = express();
+/*
 const API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
 var total = 482855;
@@ -18,11 +20,12 @@ axios.get(object_data)
     .then(response => {
         console.log(response.data);
     })
+*/
 
-bknd.get("/", function (req, res) {
-  res.sendFile(__dirname + "/Artguessr.html");
+app.get("/", function (req, res) {
+  res.send({"image":"https://images.metmuseum.org/CRDImages/ad/web-large/DP263972.jpg", "solution":"United States"});
 });
 
-bknd.listen(3000, function () {
-  console.log("Server is running on localhost3000");
+app.listen(3000, function () {
+  console.log("Server is listening on localhost:3000");
 });
