b0cc3b526317953667d1c2e5e7ed5e9f1a94b19a
Karim <kbenromd@stevens.edu>
2022-12-08 18:56:23 -0500

    new branch with methods for mongoDB leaderboard use

--- app.js
+++ app.js
@@ -13,6 +13,155 @@ const cors = require('cors');
 app.use(cors({
   origin: '*'
 }));
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://karim0820:ILJIqia1jsQk7X5s@cluster0.ah5hs8i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+//index route
+app.get("/", function(req, res){
+  res.send("ArtGuessr Streak API");
+});
+
+(async () =>{
+  let client = await MongoClient.connect(
+      uri,
+      {useNewUrlParser : true}
+  );
+
+  db = client.db("Usernames");
+
+  app.listen(port, async function(){
+    if(db){
+      console.log("Database connected");
+    }
+  });
+})();
+
+// route to create a new player
+app.post("/players", async function(req, res){
+  // get info on player from POST body data
+  let {username, games_played } = req.body;
+
+  // check if username already exists:
+  const alreadyExists = await db
+      .collection("players")
+      .findOne({username : username});
+
+  if (alreadyExists){
+    res.send({status: false, msg: "player username already exists"});
+  }else{
+    // create new player in db
+    await db.collections("players").insertOne({username: score});
+    console.log("created player with name: ${username }");
+    res.send({status: true, msg: "player created"});
+  }
+});
+
+app.put("/players", async function(req, res){
+  let {username, games_played} = req.body;
+  // check if username already exists
+
+  const alreadyExists = await db
+      .collection("players")
+      .findOne({username : username});
+  if (alreadyExists){
+    // update player object to games_played +1
+    await db
+        .collection("players")
+        .updateOne({username}, {$set:{username, games_played}});
+    console.log("player: ${username} games_played updated to ${score}")
+    res.send({status: true, msg: "player games_played updated"});
+  }else{
+    res.send({status:false, msg: "player with username could not be found"})
+  }
+});
+
+// access to leaderboard
+
+app.get("/players", async function(req, res){
+  let {lim} = req.query;
+  db.collection("players")
+      .find()
+  // -1 for descending
+      .sort({games_played: -1})
+      .limit(lim)
+      .toArray(function(err, result){
+        if(err)
+          res.send({status:false, msg: "failed to retrieve players"});
+        console.log(Array.from(result));
+        res.send({status:true, msg: result})
+      });
+});
+
+
+var output_str = "";
+
+async function run() {
+  try {
+    const database = client.db('');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// const uri = "mongodb+srv://karim0820:ILJIqia1jsQk7X5s@cluster0.ah5hs8i.mongodb.net/?retryWrites=true&w=majority";
+//
+// const client = new MongoClient(uri);
+//
+// import { MongoClient } from "mongodb";
+//
+// // Replace the uri string with your MongoDB deployment's connection string.
+// const uri = "<connection string uri>";
+//
+// const client = new MongoClient(uri);
+//
+// async function run() {
+//   try {
+//     const database = client.db("sample_mflix");
+//     const movies = database.collection("movies");
+//
+//     // create a filter for a movie to update
+//     const filter = { title: "Random Harvest" };
+//
+//     // this option instructs the method to create a document if no documents match the filter
+//     const options = { upsert: true };
+//
+//     // create a document that sets the plot of the movie
+//     const updateDoc = {
+//       $set: {
+//         plot: `A harvest of random numbers, such as: ${Math.random()}`
+//       },
+//     };
+//
+//     const result = await movies.updateOne(filter, updateDoc, options);
+//     console.log(
+//         `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+//     );
+//   } finally {
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+// async function main() {
+//   const uri = "mongodb+srv://karim0820:ILJIqia1jsQk7X5s@cluster0.ah5hs8i.mongodb.net/?retryWrites=true&w=majority";
+//   const client = new MongoClient(uri, {useNewUrlParser: true} );
+//   client.connect( err => {
+//     const collection = client.db("test").collection("devices");
+//
+//     // perform actions on the collection object
+//     client.close();
+//   });
+// }
 
 const { response } = require("express");
 const { get } = require("http");
@@ -132,6 +281,9 @@ function getArt(req, res) {
           console.log("Valid artwork requested, sent the following:");
           console.log(package);
           console.log(req.body.username);
+
+          // increment the number of times the user has played
+
         }                
     })
 
